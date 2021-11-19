b29b8e9d5faf50607b9f1ffa08fd89cb70be5a52
Alisa_Brk <free-comp@yandex.ru>
2021-11-19 16:45:53 +0300

    Added solution to the task03 (the square of the number)

---
+++ Task03/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3. По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("Задача 3. По двум заданным числам проверять, является ли одно квадратом другого.");
+
+Console.WriteLine("Введите число А: ");
+int NumbA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int NumbB = Convert.ToInt32(Console.ReadLine());
+
+if (NumbA == NumbB * NumbB)
+{
+    Console.WriteLine($"Число {NumbA} является квадратом числа {NumbB}");
+}
+else if (NumbB == NumbA * NumbA)
+{
+    Console.WriteLine($"Число {NumbB} является квадратом числа {NumbA}");
+}
+else
+{
+    Console.WriteLine("Ни одно из чисел не является квадратом другого.");
+}
